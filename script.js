004e4445169e22ccbb38b2c32b3a8af471b4febc
asal930 <arabsalehy.156@gmail.com>
2026-07-07 11:57:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 console.log("معلم هوشمند با خانم عربصالحی");
+<script>
+
+function createIdea(){
+
+let lesson =
+document.getElementById("lesson").value;
+
+let grade =
+document.getElementById("grade").value;
+
+let topic =
+document.getElementById("topic").value;
+
+let type =
+document.getElementById("type").value;
+
+
+if(topic==""){
+document.getElementById("result").innerText =
+"لطفاً موضوع درس را وارد کنید.";
+return;
+}
+
+
+let text = "";
+
+
+if(lesson=="عربی"){
+
+text =
+`برای درس عربی پایه ${grade} درباره «${topic}» یک ${type} طراحی کن. 
+از مثال‌های ساده، فعالیت کلاسی و مشارکت دانش‌آموزان استفاده شود.`;
+
+}
+
+else {
+
+text =
+`برای درس هنر پایه ${grade} درباره «${topic}» یک ${type} طراحی کن.
+ایده باید خلاقانه و قابل اجرا در کلاس باشد.`;
+
+}
+
+
+document.getElementById("result").innerText=text;
+
+}
+function copyResult(){
+
+let text = document.getElementById("result").innerText;
+
+navigator.clipboard.writeText(text);
+
+alert("نتیجه کپی شد ✅");
+
+}
+</script>
